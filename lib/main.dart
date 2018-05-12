89fbeef4e40a232de17a3bac38cf5eec7e06704c
Matt Carroll <me@mattcarroll.name>
2018-05-11 18:24:48 -0700

    Create test bed UI.

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,112 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final topLeft = "TOP_LEFT";
+  final topRight = "TOP_RIGHT";
+  final middleLeft = "MIDDLE_LEFT";
+  final middleRight = "MIDDLE_RIGHT";
+  final bottomLeft = "BOTTOM_LEFT";
+  final bottomRight = "BOTTOM_RIGHT";
+  final center = "CENTER";
+
+  bool isShowingMenu = false;
+
+  void showMenu(String location) {
+    if (!isShowingMenu) {
+      // TODO: show menu for location
+      print('Opening menu for $location');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(''),
-      ),
-      body: new Center(
+        appBar: new AppBar(
+          leading: new IconButton(
+              icon: new Icon(
+                Icons.cancel,
+              ),
+              onPressed: () {
+                showMenu(topLeft);
+              }),
+          title: new Text(''),
+          actions: <Widget>[
+            new IconButton(
+                icon: new Icon(
+                  Icons.cancel,
+                ),
+                onPressed: () {
+                  showMenu(topRight);
+                }),
+          ],
+        ),
+        body: new Stack(
+          children: <Widget>[
+            // Left center
+            new Align(
+              alignment: Alignment.centerLeft,
+              child: new IconButton(
+                  icon: new Icon(
+                    Icons.cancel,
+                  ),
+                  onPressed: () {
+                    showMenu(middleLeft);
+                  }),
+            ),
 
-      ),
-    );
+            // Left bottom
+            new Align(
+              alignment: Alignment.bottomLeft,
+              child: new IconButton(
+                  icon: new Icon(
+                    Icons.cancel,
+                  ),
+                  onPressed: () {
+                    showMenu(bottomLeft);
+                  }),
+            ),
+
+            // Right center
+            new Align(
+              alignment: Alignment.centerRight,
+              child: new IconButton(
+                  icon: new Icon(
+                    Icons.cancel,
+                  ),
+                  onPressed: () {
+                    showMenu(middleRight);
+                  }),
+            ),
+
+            // Right bottom
+            new Align(
+              alignment: Alignment.bottomRight,
+              child: new IconButton(
+                  icon: new Icon(
+                    Icons.cancel,
+                  ),
+                  onPressed: () {
+                    showMenu(bottomRight);
+                  }),
+            ),
+
+            // True center
+            new Align(
+              alignment: Alignment.center,
+              child: new IconButton(
+                  icon: new Icon(
+                    Icons.cancel,
+                  ),
+                  onPressed: () {
+                    showMenu(center);
+                  }),
+            ),
+          ],
+        ));
   }
 }
