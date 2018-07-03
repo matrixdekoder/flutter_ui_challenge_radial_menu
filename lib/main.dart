7c8b65fc6daf16d14781985caae0e60e8cadd16f
Matt Carroll <me@mattcarroll.name>
2018-07-02 23:15:47 -0700

    1. Test Bed

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Radial Menu',
       theme: new ThemeData(
+        brightness: Brightness.dark,
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(),
@@ -16,21 +17,62 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Widget _buildMenu() {
+    return IconButton(
+      icon: Icon(
+        Icons.cancel,
+      ),
+      onPressed: () {},
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(''),
+    return Scaffold(
+      appBar: AppBar(
+        leading: _buildMenu(),
+        title: Text(''),
+        actions: <Widget>[
+          _buildMenu(),
+        ],
       ),
-      body: new Center(
+      body: Stack(
+        children: <Widget>[
+          // Center
+          Align(
+            alignment: Alignment.center,
+            child: _buildMenu(),
+          ),
 
+          // Middle Left
+          Align(
+            alignment: Alignment.centerLeft,
+            child: _buildMenu(),
+          ),
+
+          // Middle Right
+          Align(
+            alignment: Alignment.centerRight,
+            child: _buildMenu(),
+          ),
+
+          // Bottom Left
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: _buildMenu(),
+          ),
+
+          // Bottom Right
+          Align(
+            alignment: Alignment.bottomRight,
+            child: _buildMenu(),
+          ),
+        ],
       ),
     );
   }
